60f924b9ecbca778fb5f255af7f734f7769b10ec
Arnav Singh <arnav@paul.local>
2026-01-08 15:46:53 +0530

    Initialize project

---
+++ src/main/java/com/arnav/week1/ApiController.java
@@ -0,0 +1,11 @@
+package com.arnav.week1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class ApiController {
+@GetMapping("/")
+public String index (){
+    return "Greetings from MIT-FIS!";
+}
+}
